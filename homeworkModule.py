87f77a7b7885661745ed0313099addde2374a499
thebooyakid <alex96740@gmail.com>
2021-06-03 20:19:32 -0400

    first commit

---
+++ homeworkModule.py
@@ -0,0 +1,7 @@
+def squareFoot(length, width):
+    area = length * width
+    print(f"The square footage of your house is {area}.")
+
+def circumference(radius):
+    circumference = 3.14 * (radius**2)
+    print(f"The circumference of your circle is {circumference}.")
